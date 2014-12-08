3d54c3bf82133b2ecdc613e44137cb827361fbec
David <davidp@thecityof.net>
2014-12-08 16:46:43 -0600

    Testing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var app = require('express')();
-var http = require('http').Server(app);
+//var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
@@ -23,6 +23,6 @@ io.on('connection', function(socket){
   
 });
 
-http.listen(3000, function(){
+app.listen(3000, function(){
   console.log('listening on *:3000');
 });

9d833fcbf7953581e93fb4add80a1805714c5b4d
David <davidp@thecityof.net>
2014-12-08 16:36:56 -0600

    Testing

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+io.on('connection', function(socket){
+  
+  console.log('a user connected');
+  
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
+  
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });

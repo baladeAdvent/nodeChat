019f81721d566f1147f2fb976845b1bec5905670
David <davidp@thecityof.net>
2014-12-09 15:16:24 -0600

    socket IO

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
-//var app = require('express')();
-//var http = require('http').Server(app);
 var express = require('express');
-var app = express();
-var io = require('socket.io')(http);
+var http = require('http');
+var sio = require('socket.io');
 
-app.set('port', (process.env.PORT || 5000));
+var server = express.createServer();
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+var port =  3000 || process.env.PORT;
+
+server.listen(port);
+var io = sio.listen(server);
+
+server.get('/',fucntion(request, response){
+	res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  
-  console.log('a user connected');
-  
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    console.log('message: ' + msg);
-  });
-  
-});
-
-app.listen(app.get('port'), function(){
-  console.log("Node app is running at localhost:" + app.get('port'));
+io.socket.on('connection',function(socket){
+	console.log('Connection on: ' + port);
 });

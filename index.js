5dab35a1b55697aefd6242ae1b7ac8d0a7fa1b5f
David <davidp@thecityof.net>
2014-12-10 11:57:07 -0600

    updating app.json

--- index.js
+++ index.js
@@ -1,37 +1,5 @@
 process.title = 'node-chat';
 
-var WebSocketServer = require('ws').server;
-var http = require('http');
-var port = (process.env.PORT || 5000);
-
-var history = [];
-var users = [];
-
-var server = http.createServer(function(request, response){
-	// Do nothing
-});
-server.listen(port,function(){});
-
-wsServer = new WebSocketServer({
-	server: server
-});
-
-/*
-wsServer.on('request',function(request){
-	var connection = request.accept(null,request.origin);
-	
-	console.log('user connected');
-	
-	connection.on('message',function(message){
-		if(message.type === 'utf8'){
-			console.log(message);
-		}
-	});
-	
-	connection.on('close',function(sonnection){
-		console.log('connection closed');
-	});
-});
 var WebSocketServer = require('ws').Server;
 var http = require('http');
 var express = require('express');
@@ -65,4 +33,3 @@ wss.on("connection", function(ws){
 		//clearInterval(id);
 	});
 });
-*/

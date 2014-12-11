2c977b1166dca825ecb220fd6ba1de8d3316f2ca
David <davidp@thecityof.net>
2014-12-11 15:06:13 -0600

    attempting to get login to work

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ var app = express();
 var port = (process.env.PORT || 5000);
 
 var chatLog = new Array();
+var userList = new Array();
 app.use(express.static(__dirname + '/'));
 
 var server = http.createServer(app);
 server.listen(port);
+
+// create empty container for data to be sent
+mdata = {};
 
 console.log("http server listening on %d", port);
 
@@ -19,9 +23,6 @@ var wss = new WebSocketServer({server: server});
 wss.on("connection", function(ws){
 	
 	//console.log(ws);
-	for(x in chatLog){
-		ws.send(chatLog[x]);
-	}
 	console.log('websocket connection open');
 	//var id = setInterval(function() {
 	//	ws.send({"keepalive"})
@@ -29,8 +30,45 @@ wss.on("connection", function(ws){
 	/////////////
 	
 	ws.onmessage = function(event){
-		chatLog.push(event.data);
-		ws.send(event.data);
+		var data = parseData(event.data);
+		console.log(data);
+		switch(data['type']){
+			case 'ping':
+				console.log('ping from user:' + data['username'] );
+				break;
+				
+			case 'chat message':
+				mdata = {
+					'type': 'chat message',
+					'username': data['username'],
+					'message': data['message']
+				}
+				chatLog.push(mdata);
+				ws.send(stringify(mdata));
+				break;
+				
+			case 'log request':
+				console.log('Log request');
+				for(x in chatLog){				
+					mdata = {
+						'type': 'log message',
+						'username': chatLog[x]['username'],
+						'message': chatLog[x]['message']
+					}
+					ws.send(stringify(mdata));
+				}
+				break;
+				
+			case 'disconnect':
+				mdata = {
+					'type': 'system message',
+					'username': data['username'],
+					'message': ' has disconnected from the chat...'
+				}
+				chatLog.push(mdata);
+				ws.send(stringify(mdata));
+				break;
+		}
 	};
 	
 	/////////////
@@ -39,3 +77,24 @@ wss.on("connection", function(ws){
 		//clearInterval(id);
 	});
 });
+
+function stringify(obj){
+	output = '';
+	for(x in obj){
+		output += x + '~:~' + obj[x] + '}:}';
+	}
+	return output.replace(/}:}$/,'');
+}
+
+function parseData(str){
+	var Obj = [];
+	
+	var array1 =  str.split('}:}');
+	for(x in array1){
+		parts = array1[x].split('~:~');
+		if(parts[0] != ''){
+			Obj[parts[0]] = parts[1];
+		}
+	}
+	return Obj
+}

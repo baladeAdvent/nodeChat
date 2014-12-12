7794b027e9617117f8a12f9604ea3394b98b6f93
David A Perez <davidp@thecityof.net>
2014-12-11 20:54:08 -0600

    make it better

--- functions.js
+++ functions.js
@@ -1,29 +1,34 @@
-		function stringify(obj){
-			output = '';
-			for(x in obj){
-				output += x + '~:~' + obj[x] + '}:}';
-			}
-			return output.replace(/}:}$/,'');
+function stringify(obj){
+	output = '';
+	for(x in obj){
+		output += x + '~:~' + obj[x] + '}:}';
+	}
+	return output.replace(/}:}$/,'');
+}
+
+function scrollChat(){
+		chatbox = $('#nodeChat_messages').height();
+		console.log(chatbox);
+}
+//////////////////////////////
+function parseData(str){
+	var Obj = [];
+	
+	var array1 =  str.split('}:}');
+	for(x in array1){
+		parts = array1[x].split('~:~');
+		if(parts[0] != ''){
+			Obj[parts[0]] = parts[1];
 		}
-		
-		function parseData(str){
-			var Obj = [];
-			
-			var array1 =  str.split('}:}');
-			for(x in array1){
-				parts = array1[x].split('~:~');
-				if(parts[0] != ''){
-					Obj[parts[0]] = parts[1];
-				}
-			}
-			return Obj;
-		}
-		
-		function trim(str){
-			var pattern = /^( ){1,}|( ){1,}$/;
-			return str.replace(pattern,'');
-		}
-		
-		function log(msg){
-			console.log(msg);
-		}
+	}
+	return Obj;
+}
+
+function trim(str){
+	var pattern = /^( ){1,}|( ){1,}$/;
+	return str.replace(pattern,'');
+}
+
+function log(msg){
+	console.log(msg);
+}

7bb0348f7a0d72498fbbd861fb5b421c9bdc1029
David A Perez <davidp@thecityof.net>
2014-12-11 21:13:52 -0600

    make it better

--- functions.js
+++ functions.js
@@ -9,8 +9,8 @@ function stringify(obj){
 function scrollChat(scrollStatus){
 	console.log('Scroll Chat...' + scrollStatus);
 	if(scrollStatus === true){
-		console.log(el.prop('scrollHeight'));
 		el = $('#nodeChat_message');
+		console.log(el.prop('scrollHeight'));
 		el.animate({ sctrollTop:el.prop('scrollHeight')},1000);
 	}
 }

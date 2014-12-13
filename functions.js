a80a3752d9b0dc5188016d81e47f120566356086
David A Perez <davidp@thecityof.net>
2014-12-13 12:48:55 -0600

    text animation

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 //////////////////////////////
-function appendToChat(str){
+function appendToChat(str,scrollStatus){
 	var message = $('<li>').html(str).css('display','none');
-	$('#nodeChat_messages').append(	message.slideDown(1000) );
+	$('#nodeChat_messages').append(	message.slideDown(1000,function((scroll){ scrollChat(scroll) }) );
 }
 //////////////////////////////
 function scrollChat(scrollStatus){

edf7275ce004b18f60f8f8ab20610864ef842028
David <davidp@thecityof.net>
2014-12-31 13:11:46 -0600

    display current color in options at start...?

--- functions.js
+++ functions.js
@@ -24,7 +24,7 @@ function updateUserlist(json){
 			user = list[x];
 			logProperties(list[x]);
 			des.append( $('<li style="color:rgb(' + user['color'] + ');">').html(user['name']) );
-			$('#textColor').val(user['color']);
+			$('#textColor').val(user['color']).css('background-color','rgb(' + user['color'] + ')');
 		}
 }
 //////////////////////////////

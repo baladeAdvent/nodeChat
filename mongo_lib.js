14f9d1fd9bcd717f8e00703f32e4676a95bf5a69
David <davidp@thecityof.net>
2015-01-23 11:05:17 -0600

    attemtping to get exported function to work...

--- mongo_lib.js
+++ mongo_lib.js
@@ -6,23 +6,9 @@ var MONGO_USER = 'nodechatsystem';
 var MONGO_PASS = 'nodechat123456nodechat';
 var MONGO_DB = 'nodechattest';
 
-exports.open = function(){
-	MongoClient.connect("mongodb://" + MONGO_USER + ":" + MONGO_PASS + "@ds031661.mongolab.com:31661/" + MONGO_DB ,function(err, db){
-		if(!err) {
-			console.log("Connected to MongoDB");
-		}else{
-			console.log("Unable to connect to MongoDB");
-		}
-		
-		var collection = db.collection('users');
-		collection.findOne({'username':'admin'},function(err,item){
-		console.log(item);
-		});
-		
-	});
-}
 
 exports.checkUsername = function(name){
+	console.log('Check Username');
 	MongoClient.connect("mongodb://" + MONGO_USER + ":" + MONGO_PASS + "@ds031661.mongolab.com:31661/" + MONGO_DB ,function(err, db){
 		if(!err) {
 			return false;

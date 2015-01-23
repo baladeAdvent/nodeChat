eda039207bcd04c392f15a7cd5e0e12d331cc655
David <davidp@thecityof.net>
2015-01-23 11:08:16 -0600

    fixed the problem.... testing the function again...

--- mongo_lib.js
+++ mongo_lib.js
@@ -6,17 +6,32 @@ var MONGO_USER = 'nodechatsystem';
 var MONGO_PASS = 'nodechat123456nodechat';
 var MONGO_DB = 'nodechattest';
 
+exports.open = function(){
+	MongoClient.connect("mongodb://" + MONGO_USER + ":" + MONGO_PASS + "@ds031661.mongolab.com:31661/" + MONGO_DB ,function(err, db){
+		if(!err) {
+			console.log("Connected to MongoDB");
+		}else{
+			console.log("Unable to connect to MongoDB");
+		}
+		
+		var collection = db.collection('users');
+		collection.findOne({'username':'admin'},function(err,item){
+		console.log(item);
+		});
+		
+	});
+}
 
 exports.checkUsername = function(name){
 	console.log('Check Username');
 	MongoClient.connect("mongodb://" + MONGO_USER + ":" + MONGO_PASS + "@ds031661.mongolab.com:31661/" + MONGO_DB ,function(err, db){
-		if(!err) {
+		if(err) {
 			return false;
 		}
 		
 		var collection = db.collection('users');
 		collection.findOne({'username':name},function(err,item){
-		console.log(name + ': ' + item);
+		console.log(name + '(' + item.length + '): ' + item);
 		});
 		
 	});

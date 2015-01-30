2025175a325ffb120f66c3c33f533a6b7a886d72
David <davidp@thecityof.net>
2015-01-30 14:47:45 -0600

    quick fix

--- functions.js
+++ functions.js
@@ -199,7 +199,7 @@ $(document).ready(function(){
 		});
 
 		if(validationStatus == false){
-			message = $('<div></div>').attr('class','alert alert-danger').html('Please fill out all form fields to login
+			message = $('<div></div>').attr('class','alert alert-danger').html('Please fill out all form fields to login');
 			$('#nodeChat_loginResponse').html('').append( message ).hide().animate({height:'show'},500).delay(8000).animate({height:'hide'},500);
 		}else{
 			if( trim($('#nodeChat_login_password').val()) == ''){

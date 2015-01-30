671824a7b7a43a22d13cd6e374aeeeec71688088
David <davidp@thecityof.net>
2015-01-30 11:14:56 -0600

    phantom dance the night away

--- functions.js
+++ functions.js
@@ -79,10 +79,10 @@ $(document).ready(function(){
 			return false;
 		},
 		onChange: function (hsb, hex, rgb) {
-			$('#colorpickerSwatch div').css('backgroundColor', '#' + hex);
+			$('#textColor div').css('backgroundColor', '#' + hex);
 		},
 		onSubmit: function(hsb, hex, rgb, el){
-			$('#colorpickerSwatch div').css('backgroundColor', '#' + hex);
+			$('#textColor div').css('backgroundColor', '#' + hex);
 			updateTextColor(rgb);
 			$(el).ColorPickerHide();
 		}
